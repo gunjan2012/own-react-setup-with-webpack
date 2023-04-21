b847e8571ddb44d7241ae78cdd73e63991cd1872
gunjan2012 <gunjan.d@simformsolutions.com>
2023-04-21 18:02:59 +0530

    other hooks

--- src/components/MemoHook.js
+++ src/components/MemoHook.js
@@ -1,24 +1,32 @@
-import React, { useState } from "react";
-import Button from "./Button";
-import Title from "./Title";
-import Count from "./Count";
-function ParentComponent() {
-  const [age, setAge] = useState(25);
-  const [salary, setSalary] = useState(25000);
-  const incrementAge = () => {
-    setAge(age + 1);
+import React, { useState, useMemo } from "react";
+function Counter() {
+  const [counterone, setCounterOne] = useState(0);
+  const [countertwo, setCounterTwo] = useState(0);
+  const incrementOne = () => {
+    setCounterOne(counterone + 1);
   };
-  const incrementSalary = () => {
-    setSalary(salary + 1000);
+  const incrementTwo = () => {
+    setCounterTwo(countertwo + 1);
   };
+  // const isEven = () => {
+  //   let i = 0;
+  //   while (i < 200000) i++;
+  //   return counterone % 2 === 0;
+  // }; // by doing this both buttons get affected a delay because
+  // this will re render whole component and so to avoid we can add this function in useMemo and we can assign this it to variable .
+
+  const isEven = useMemo(() => {
+    let i = 0;
+    while (i < 200000) i++;
+    return counterone % 2 === 0;
+  }, [counterone]);
+
   return (
     <div>
-      <Title />
-      <Count text="age" count={age} />
-      <Button handleClick={incrementAge}>Increment my age</Button>
-      <Count text="salary" count={salary} />
-      <Button handleClick={incrementSalary}>Increment my salary</Button>
+      <button onClick={incrementOne}>Count one - {counterone}</button>         
+      <button onClick={incrementTwo}>Count two - {countertwo}</button>{" "}
+      <span>{isEven ? "Even" : "odd"}</span>
     </div>
   );
 }
-export default ParentComponent;
+export default Counter;
